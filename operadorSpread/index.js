3f2cc32f17b90bfba12760c698ea0ecfe2fb05c4
Emanuel <emanuelzsmj@gmail.com>
2022-11-29 13:18:33 -0300

    Modulo 3 do Bootcamp JS Onebitcode

---
+++ operadorSpread/index.js
@@ -0,0 +1,7 @@
+const person = ['lixo', 'feia', 'nojenta', 'mentirosa', 'falsa', 'burra']
+const personClone = [...person]
+
+console.log(personClone)
+personClone.push('Teste')
+
+console.log(person, personClone)
