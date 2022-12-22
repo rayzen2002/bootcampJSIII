8ff37bbc1b589beb65cab4e32b025dd261294268
Emanuel <emanuelzsmj@gmail.com>
2022-12-22 19:31:48 -0300

    Conhecendo o babel

---
+++ usando-o-babel/dist/index.js
@@ -0,0 +1,65 @@
+"use strict";
+
+var media = function media() {
+  for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+  var sum = args.reduce(function (accum, num) {
+    return accum + num;
+  }, 0);
+  return sum / args.length;
+};
+var mediaPonderada = function mediaPonderada() {
+  for (var _len2 = arguments.length, args = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+    args[_key2] = arguments[_key2];
+  }
+  var sum = args.reduce(function (accum, _ref) {
+    var number = _ref.number,
+      weight = _ref.weight;
+    return accum + number * (weight !== null && weight !== void 0 ? weight : 1);
+  }, 0);
+  var weightSum = args.reduce(function (accum, arg) {
+    var _arg$weight;
+    return accum + ((_arg$weight = arg.weight) !== null && _arg$weight !== void 0 ? _arg$weight : 1);
+  }, 0);
+  return sum / weightSum;
+};
+console.log("M\xE9dia Ponderada: ".concat(mediaPonderada({
+  number: 9,
+  weight: 3
+}, {
+  number: 7
+}, {
+  number: 10,
+  weight: 1
+})));
+var median = function median() {
+  for (var _len3 = arguments.length, numbers = new Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
+    numbers[_key3] = arguments[_key3];
+  }
+  var orderedNumbers = [].concat(numbers).sort(function (a, b) {
+    return a - b;
+  });
+  var middle = Math.floor(orderedNumbers.length / 2);
+  if (orderedNumbers.length % 2 !== 0) {
+    return orderedNumbers[middle];
+  }
+  var firstMedian = orderedNumbers[middle - 1];
+  var secondMedian = orderedNumbers[middle];
+  return media(firstMedian, secondMedian);
+};
+var mode = function mode() {
+  for (var _len4 = arguments.length, numbers = new Array(_len4), _key4 = 0; _key4 < _len4; _key4++) {
+    numbers[_key4] = arguments[_key4];
+  }
+  // [ [n, qtd], [n, qtd], [n, qtd] ]
+  var quantities = numbers.map(function (num) {
+    return [num, numbers.filter(function (n) {
+      return num === n;
+    }).length];
+  });
+  quantities.sort(function (a, b) {
+    return b[1] - a[1];
+  });
+  return quantities[0][0];
+};
